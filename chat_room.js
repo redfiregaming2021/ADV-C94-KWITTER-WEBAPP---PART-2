3c64e44749b12d5ace497403467c8994a26e6ab1
redfiregaming2021 <84076916+redfiregaming2021@users.noreply.github.com>
2021-05-12 16:44:25 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,13 @@
+function getData() {
+    firebase.database().ref("/").on('value', function(snapshot) {
+        document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+    Room_names = childKey;
+   //Start code
+
+   //End code
+   });});}
+getData();
+function addRoom() {
+    user = document.getElementById("room_name").value;
+    window.location(rooms.html);
+}
